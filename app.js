0b37621ade575f53c9457ce24457ad75b42891b6
Sahil Tiwari <sahiltiwari.9898@gmail.com>
2021-03-18 16:33:14 +0530

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+require('dotenv/config');
 const app = express();
 
 
@@ -11,7 +12,10 @@ app.get('/', (_, res) => {
 
 
 
-
+//connection to DB
+mongoose.connect(process.env.DB_CONNECTION, () =>{
+    console.log("Connected to database");
+},{useNewUrlParser: true } );
 
 
 //starting the server on localhost
