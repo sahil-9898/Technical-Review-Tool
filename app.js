93e84cc8b200c934d1ff8d63ddd1a0322c2478ce
Sahil Tiwari <sahiltiwari.9898@gmail.com>
2022-06-02 14:42:29 +0530

    bug fixes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { urlencoded } = require("body-parser");
+require("dotenv/config");
 const dbConnect = require("./db");
 const users = require("./models/users");
 const reviews = require("./models/reviews.js");
@@ -7,8 +8,8 @@ const session = require("express-session");
 const redis = require("redis");
 const redisConnect = require("connect-redis");
 const app = express();
-require("dotenv/config");
-dbConnect();
+
+// dbConnect();
 
 app.use(urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -16,18 +17,18 @@ app.use(express.static("public"));
 let RedisStore = redisConnect(session);
 let redisClient = redis.createClient();
 app.use(
-  session({
-    name: "eid",
-    store: new RedisStore({ client: redisClient, disableTouch: true }),
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
-      httpOnly: true,
-      sameSite: "lax",
-    },
-    secret: process.env.SECRET,
-    resave: false,
-  })
+    session({
+        name: "eid",
+        store: new RedisStore({ client: redisClient, disableTouch: true }),
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+            httpOnly: true,
+            sameSite: "lax",
+        },
+        secret: process.env.SECRET,
+        resave: false,
+    })
 );
 
 // let currentUser = "abcd";
@@ -35,182 +36,186 @@ let error = "none";
 
 //---------------routes-------------------------------------------
 app.get("/", (req, res) => {
-  if (!req.session.currentUser) res.render("index.ejs", { error: "none" });
-  else res.redirect("/reviews");
+    if (!req.session.currentUser) res.render("index.ejs", { error: "none" });
+    else res.redirect("/reviews");
 });
 
 app.post("/", (req, res) => {
-  const eid = req.body.eid;
-  users.findOne({ eid: eid }, (err, user) => {
-    if (err) {
-      console.log(err);
-      res.render("index.ejs", { error: "Invalid Employee ID" });
-    } else {
-      if (user == null) {
-        res.render("index.ejs", { error: "Invalid Employee ID" });
-      } else {
-        req.session.currentUser = user.designation;
-        // currentUser = user.designation;
-        res.redirect("/reviews");
-      }
-    }
-  });
+    const eid = req.body.eid;
+    users.findOne({ eid: eid }, (err, user) => {
+        if (err) {
+            console.log(err);
+            res.render("index.ejs", { error: "Invalid Employee ID" });
+        } else {
+            if (user == null) {
+                res.render("index.ejs", { error: "Invalid Employee ID" });
+            } else {
+                req.session.currentUser = user.designation;
+                // currentUser = user.designation;
+                res.redirect("/reviews");
+            }
+        }
+    });
 });
 
 app.get("/reviews", (req, res) => {
-  if (!req.session.currentUser) {
-    res.redirect("/");
-  } else {
-    reviews.find({}, (err, reviews) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("reviews.ejs", {
-          reviews: reviews,
-          error: error,
+    if (!req.session.currentUser) {
+        res.redirect("/");
+    } else {
+        reviews.find({}, (err, reviews) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("reviews.ejs", {
+                    reviews: reviews,
+                    error: error,
+                });
+                error = "none";
+            }
         });
-        error = "none";
-      }
-    });
-  }
+    }
 });
 
 app.get("/reviews/:id", (req, res) => {
-  if (!req.session.currentUser) {
-    res.redirect("/");
-  } else {
-    reviews.findById(req.params.id, (err, review) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("reviewDetails.ejs", { review: review, error: error });
-        error = "none";
-      }
-    });
-  }
+    if (!req.session.currentUser) {
+        res.redirect("/");
+    } else {
+        reviews.findById(req.params.id, (err, review) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("reviewDetails.ejs", {
+                    review: review,
+                    error: error,
+                });
+                error = "none";
+            }
+        });
+    }
 });
 
 app.get("/addChanges/:id/:comment", (req, res) => {
-  if (req.session.currentUser === "developer") {
-    reviews.findById(req.params.id, (err, review) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("addChanges.ejs", {
-          comment: review.panelMembers[req.params.comment],
-          id: req.params.id,
-          commentIndex: req.params.comment,
-          review: review,
+    if (req.session.currentUser === "developer") {
+        reviews.findById(req.params.id, (err, review) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("addChanges.ejs", {
+                    comment: review.panelMembers[req.params.comment],
+                    id: req.params.id,
+                    commentIndex: req.params.comment,
+                    review: review,
+                });
+            }
         });
-      }
-    });
-  } else {
-    console.log("You Do not have permission to add changes");
-    error = "You do not have permission to use this feature";
-    res.redirect("/reviews/" + req.params.id);
-  }
+    } else {
+        console.log("You Do not have permission to add changes");
+        error = "You do not have permission to use this feature";
+        res.redirect("/reviews/" + req.params.id);
+    }
 });
 
 app.post("/addChanges/:id/:comment", (req, res) => {
-  reviews.findById(req.params.id, async (err, review) => {
-    if (err) {
-      console.log(err);
-    } else {
-      review.panelMembers[req.params.comment].comment.changeDesc =
-        req.body.changeDesc;
-      review.panelMembers[req.params.comment].comment.commitLink =
-        req.body.commitLink;
-      review.panelMembers[req.params.comment].comment.status = "Acknowledged";
-      await review.save();
-      res.redirect("/reviews/" + req.params.id);
-    }
-  });
+    reviews.findById(req.params.id, async (err, review) => {
+        if (err) {
+            console.log(err);
+        } else {
+            review.panelMembers[req.params.comment].comment.changeDesc =
+                req.body.changeDesc;
+            review.panelMembers[req.params.comment].comment.commitLink =
+                req.body.commitLink;
+            review.panelMembers[req.params.comment].comment.status =
+                "Acknowledged";
+            await review.save();
+            res.redirect("/reviews/" + req.params.id);
+        }
+    });
 });
 
 app.get("/viewChanges/:id/:comment", (req, res) => {
-  reviews.findById(req.params.id, (err, review) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("viewChanges.ejs", {
-        review: review,
-        comment: review.panelMembers[req.params.comment],
-      });
-    }
-  });
+    reviews.findById(req.params.id, (err, review) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("viewChanges.ejs", {
+                review: review,
+                comment: review.panelMembers[req.params.comment],
+            });
+        }
+    });
 });
 
 app.get("/reviewForm", (req, res) => {
-  if (req.session.currentUser == "manager") {
-    res.render("reviewForm1.ejs");
-  } else {
-    console.log("You do not have permission to conduct a review");
-    error = "You do not have permission to use this feature";
-    res.redirect("/reviews");
-  }
+    if (req.session.currentUser == "manager") {
+        res.render("reviewForm1.ejs");
+    } else {
+        console.log("You do not have permission to conduct a review");
+        error = "You do not have permission to use this feature";
+        res.redirect("/reviews");
+    }
 });
 
 app.post("/reviewForm", (req, res) => {
-  if (!req.session) {
-    res.redirect("/");
-  } else {
-    const form1 = req.body;
-    reviews.create(
-      {
-        name: form1.reviewName,
-        objective: form1.objective,
-        panelMembers: [
-          { name: form1.name[0] },
-          { name: form1.name[1] },
-          { name: form1.name[2] },
-          { name: form1.name[3] },
-          { name: form1.name[4] },
-        ],
-      },
-      (err, form1) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.redirect("/reviewForm/" + form1._id);
-        }
-      }
-    );
-  }
+    if (!req.session) {
+        res.redirect("/");
+    } else {
+        const form1 = req.body;
+        reviews.create(
+            {
+                name: form1.reviewName,
+                objective: form1.objective,
+                panelMembers: [
+                    { name: form1.name[0] },
+                    { name: form1.name[1] },
+                    { name: form1.name[2] },
+                    { name: form1.name[3] },
+                    { name: form1.name[4] },
+                ],
+            },
+            (err, form1) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.redirect("/reviewForm/" + form1._id);
+                }
+            }
+        );
+    }
 });
 
 app.get("/reviewForm/:id", (req, res) => {
-  const id = req.params.id;
-  reviews.findById(id, (err, review) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("reviewForm2.ejs", { review: review });
-    }
-  });
+    const id = req.params.id;
+    reviews.findById(id, (err, review) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("reviewForm2.ejs", { review: review });
+        }
+    });
 });
 
 app.post("/reviewForm/:id", (req, res) => {
-  reviews.findOne({ _id: req.params.id }, (err, review) => {
-    if (err) {
-      console.log(err);
-    } else {
-      for (let i = 0; i < 5; i++) {
-        review.panelMembers[i].comment.comment = req.body.comments[i];
-        review.panelMembers[i].comment.severity = req.body.severity[i];
-      }
-      review.save();
-      res.redirect("/reviews");
-    }
-  });
+    reviews.findOne({ _id: req.params.id }, (err, review) => {
+        if (err) {
+            console.log(err);
+        } else {
+            for (let i = 0; i < 5; i++) {
+                review.panelMembers[i].comment.comment = req.body.comments[i];
+                review.panelMembers[i].comment.severity = req.body.severity[i];
+            }
+            review.save();
+            res.redirect("/reviews");
+        }
+    });
 });
 
 app.get("/logout", (req, res) => {
-  // currentUser = "";
-  req.session.destroy();
-  res.redirect("/");
+    // currentUser = "";
+    req.session.destroy();
+    res.redirect("/");
 });
 
 //starting the server on localhost
 app.listen(3000, () => {
-  console.log("Server started on localhost 3000");
+    console.log("Server started on localhost 3000");
 });
--- db.js
+++ db.js
@@ -1,13 +1,16 @@
 const Mongoose = require("mongoose");
-require("dotenv/config");
 
 //connection to DB
-module.exports = async () => {
-  await Mongoose.connect(
-    process.env.DB_CONNECTION,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log("database connected");
-    }
-  );
+const db = async () => {
+    await Mongoose.connect(
+        process.env.DB_CONNECTION,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+        () => {
+            console.log("DB connected");
+        }
+    );
 };
+db();
